09fe2c5dce027dbd319cc1fe89a4966f212ede57
juan gabriel rios <juangrios27@gmail.com>
2021-10-13 12:57:32 -0300

    desafio 2 - metodos, texturas

--- Assets/Scripts/box.cs
+++ Assets/Scripts/box.cs
@@ -6,9 +6,9 @@ public class box : MonoBehaviour
 {
     public Vector3 positionBox = new Vector3(2, 0.5f, -2.4f);
     public Vector3 scaleBox = new Vector3(1, 1, 1);
-
-    public Vector3 changePositionBox = new Vector3(0, 0, 0.01f);
-    public Vector3 changeScaleBox = new Vector3(0.0001f, 0.0001f, 0.0001f);
+    public float speedBox = 10;
+    public Vector3 changePositionBox = new Vector3(0, 0, 0.1f);
+    public Vector3 changeScaleBox = new Vector3(0.001f, 0.001f, 0.001f);
 
     // Start is called before the first frame update
     void Start()
@@ -20,7 +20,7 @@ public class box : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position += changePositionBox;
+        transform.position += changePositionBox * speedBox * Time.deltaTime;
         transform.localScale -= changeScaleBox;
 
     }
--- Assets/Scripts/flyBox.cs
+++ Assets/Scripts/flyBox.cs
@@ -5,12 +5,13 @@ using UnityEngine;
 public class flyBox : MonoBehaviour
 {
     public string namePlayer = "Box Birdy";
+    public float lifePlayer = 1000;
+    public float changeLife = 5;
+    public float speedCuration = 10;
+    public float speedPlayer = 5;
     public Vector3 position1 = new Vector3(-1.65f, -0.219f, -6.22f);
-  
-
-    public Vector3 changePosition = new Vector3(0, 0.001f, 0.01f);
+    public Vector3 changePosition = new Vector3(0, 0.01f, 0.1f);
     
-
     // Start is called before the first frame update
     void Start()
     {
@@ -21,8 +22,17 @@ public class flyBox : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position += changePosition;
-   
-        
+        move();
+        // cure();
+        damage();
+    }
+    void move(){
+        transform.position += changePosition * speedPlayer * Time.deltaTime;
+    }
+    void cure(){
+        lifePlayer += changeLife * speedCuration * Time.deltaTime;
+    }
+    void damage(){
+        lifePlayer -= changeLife * speedCuration * Time.deltaTime;
     }
 }
