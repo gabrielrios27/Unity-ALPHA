4f8e583a0a7f30cc88c8a7ddfa52fa0d4ce3676b
juan gabriel rios <juangrios27@gmail.com>
2021-10-20 19:55:11 -0300

    trabajando en las balas con prefabs

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+   
+
+    public float speedEnemy = 10;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(speedEnemy * Time.deltaTime * Vector3.back);
+    }
+}
